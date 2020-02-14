350a108f1ac56a2f7afbecf426d2581cf2883bbe
cherkaoui <cherkaoui.mouad@live.fr>
2020-02-14 22:48:40 +0100

    Up: swagger configuration complete

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,12 +6,25 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
+  const options = new   DocumentBuilder()
+    .setTitle('Gemo-Challenge Api')
+    .setDescription('Gemography Challenge api over NestJs server')
+    .setVersion('1.0')
+    .addTag('gemography')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.port || 3333;
+
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
