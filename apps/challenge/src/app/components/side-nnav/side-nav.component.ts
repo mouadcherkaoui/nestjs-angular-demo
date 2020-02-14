da34f4acd1a8194284180eb86a83914a872ec6b1
cherkaoui <cherkaoui.mouad@live.fr>
2020-02-14 22:21:44 +0100

    Fix: sidenav container

--- apps/challenge/src/app/components/side-nnav/side-nav.component.ts
+++ apps/challenge/src/app/components/side-nnav/side-nav.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
-  selector: 'gemography-side-nnav',
+  selector: 'gemography-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.css'],
 })
 export class SideNavComponent implements OnInit {
 
