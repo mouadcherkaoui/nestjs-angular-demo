d31e2740ece4f65a23d7a12465a7e75a77f1c494
cherkaoui <cherkaoui.mouad@live.fr>
2020-02-16 11:26:50 +0100

    Up: using ExpressAdapter to serve from firebase

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,13 +4,20 @@
  */
 
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
-import { AppModule } from './app/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+import * as express from 'express';
+import * as functions from 'firebase-functions'
+
+import { AppModule } from './app/app.module';
+import { INestApplication } from '@nestjs/common';
+
+const globalPrefix = 'api';
+const port = process.env.port || 3333;
+
+function configureAppMiddlewares(app: INestApplication){
 
   app.setGlobalPrefix(globalPrefix);
   const options = new   DocumentBuilder()
@@ -24,7 +31,12 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
-  const port = process.env.port || 3333;
+}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  configureAppMiddlewares(app);
 
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
@@ -32,3 +44,22 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+const expressServer = express();
+
+const createFunction =
+  async (expressInstance): Promise<void> =>{
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressInstance));
+
+    configureAppMiddlewares(app);
+
+    await app.init();
+  };
+
+createFunction(expressServer)
+  .then(()=> console.log("Nest is ready"))
+  .catch(err => console.error("Something went wrong!! ", err));
+
+export const api = functions.https.onRequest(expressServer);
